8c60c203f2fedb52999f295d12a151ed922a9451
MarvinZ <marvinzzz@gmail.com>
2023-07-27 14:01:54 -0600

    added code to display result in real time

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -4,19 +4,31 @@ import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent {
-  data: any[] =[];
+  data: any[] = [];
   headers: string[] = [];
-  columns: { name: string, prop: string }[] = [];  // Initialize columns as an empty array
-  resultHeaders = ["UPC", "COST", "ITEM NAME", "ITEM CODE", "SIZE/ TYPE/ VARIATION", "CASE PACK"];
+  columns: { name: string; prop: string }[] = []; // Initialize columns as an empty array
+  resultHeaders = [
+    'UPC',
+    'COST',
+    'ITEM NAME',
+    'ITEM CODE',
+    'SIZE/ TYPE/ VARIATION',
+    'CASE PACK',
+  ];
   selectedColumns: string[] = [];
+  selectedDataObj: { [key: string]: any }[] = []; // Initialize as an empty array
+  selectedColumnsObj: { name: string; prop: string }[] = [];
 
+  constructor() {
+    this.selectedColumnsObj = this.resultHeaders.map(header => ({ name: header, prop: header }));
+  }
 
 
   onFileChange(evt: any) {
-    const target: DataTransfer = <DataTransfer>(evt.target);
+    const target: DataTransfer = <DataTransfer>evt.target;
     if (target.files.length !== 1) throw new Error('Cannot use multiple files');
 
     const reader: FileReader = new FileReader();
@@ -37,14 +49,15 @@ export class UploadComponent {
         json.shift();
       }
 
-      this.headers = json.length ? json.shift() as string[] : [];
+      this.headers = json.length ? (json.shift() as string[]) : [];
       if (this.headers.length > 0) {
-        this.columns = this.headers.filter(header => header !== null && header !== undefined)
-        .map(header => ({ name: header, prop: header }));
+        this.columns = this.headers
+          .filter((header) => header !== null && header !== undefined)
+          .map((header) => ({ name: header, prop: header }));
       }
 
       /* transform rows into objects */
-      this.data = json.map(row => {
+      this.data = json.map((row) => {
         let rowData: { [key: string]: any } = {};
         this.headers.forEach((header, i) => {
           if (header && i < row.length) {
@@ -54,28 +67,53 @@ export class UploadComponent {
         return rowData;
       });
 
+      /* Update selectedData based on selectedColumns */
+      this.selectedDataObj = this.data.map((row) => {
+        let rowData: { [key: string]: any } = {};
+        this.resultHeaders.forEach((header, i) => {
+          if (
+            this.selectedColumns[i] &&
+            row[this.selectedColumns[i]] !== undefined
+          ) {
+            rowData[header] = row[this.selectedColumns[i]];
+          }
+        });
+        return rowData;
+      });
+
       /* log headers and data */
       console.log('Headers:', this.headers);
       console.log('Data:', this.data);
+      console.log('Selected Data:', this.selectedData);
     };
 
     reader.readAsBinaryString(target.files[0]);
   }
 
-
-
   private isValidHeader(row: any[]): boolean {
-    const nonEmptyCells = row.filter(cell => cell !== null && cell !== undefined && cell !== '');
-    const threshold = 5;  // Adjust this value as needed
+    const nonEmptyCells = row.filter(
+      (cell) => cell !== null && cell !== undefined && cell !== ''
+    );
+    const threshold = 5; // Adjust this value as needed
     return nonEmptyCells.length >= threshold;
   }
 
+  updateSelectedData() {
+    this.selectedDataObj = this.data.map((row) => {
+      let selectedRow: { [key: string]: any } = {};
+      this.resultHeaders.forEach((resultHeader, i) => {
+        let selectedColumn = this.selectedColumns[i];
+        selectedRow[resultHeader] = selectedColumn ? row[selectedColumn] : null;
+      });
+      return selectedRow;
+    });
 
-
-
-
-
-
+    // Update selectedColumnsObj to match resultHeaders
+    this.selectedColumnsObj = this.resultHeaders.map((header) => ({
+      name: header,
+      prop: header,
+    }));
+  }
 
   download() {
     /* Create a new workbook and worksheet */
@@ -90,7 +128,7 @@ export class UploadComponent {
   }
 
   selectedData() {
-    return this.data.map(row => {
+    return this.data.map((row) => {
       let selectedRow: { [key: string]: any } = {};
       this.resultHeaders.forEach((header, i) => {
         selectedRow[header] = row[this.selectedColumns[i]];
@@ -98,7 +136,4 @@ export class UploadComponent {
       return selectedRow;
     });
   }
-
-
-
 }
